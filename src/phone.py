33e757c9ff4e575597ce02ce973e37477c617a36
ocean88 <oceann3@gmail.com>
2023-12-31 13:02:23 +0900

    4 добавлен декоторатор для проверки сим

--- src/phone.py
+++ src/phone.py
@@ -17,6 +17,12 @@ class Phone(Item):
     def __repr__(self):
         return f"Phone({repr(self.name)}, {self.price}, {self.quantity}, {self.number_of_sim})"
 
-    def check_sim(self):
-        if self.number_of_sim < 1:
-            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if value not in [1, 2]:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
+        self._number_of_sim = value
